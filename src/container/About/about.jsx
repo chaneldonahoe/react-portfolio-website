d9894d553e11ded982a15ac5bd384c242015122c
chanel donahoe <cdonahoe05@gmail.com>
2022-12-23 11:29:18 +1100

    added in backend folder

--- src/container/About/about.jsx
+++ src/container/About/about.jsx
@@ -17,7 +17,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that <span>Good Design</span><br /> means <span>Good Business</span>
+        I know that <span>G</span><br /> means <span>Good Business</span>
       </h2>
 
       <div className='app__profiles'>
